b29f770442e031a35e63b31c8e5d6faa8ea17cca
Alexis Sukrieh <sukria@adama.(none)>
2009-08-04 01:27:12 +0200

    all pages done except ABOUT

--- perldancer.pl
+++ perldancer.pl
@@ -3,8 +3,21 @@
 use Dancer;
 use Template;
 
+my @pages = qw(quickstart documentation contribute about);
+
 get '/' => sub {
-    template "home"
+    template 'home';
+};
+
+get '/:page' => sub {
+    my ($page) = params->{page};
+    pass and return false unless grep /$page/, @pages;
+    template $page;
+};
+
+get '/*' => sub {
+    status 'not_found';
+    template 'not_found';
 };
 
 dance;
