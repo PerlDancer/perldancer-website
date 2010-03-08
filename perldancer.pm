ab424392d91f8e11eef01fe5e30be4a211ab792f
Alexis Sukrieh <sukria@sukria.net>
2010-03-08 08:30:54 +0100

    removed most of the code thanks to new feature 'auto_page'; w00t

--- perldancer.pm
+++ perldancer.pm
@@ -2,21 +2,12 @@ package perldancer;
 use Dancer;
 use Template;
 
-my @pages = qw(quickstart documentation contribute about example download donate);
-
 get '/' => sub {
     template 'home';
-};
-
-get '/:page' => sub {
-    my ($page) = params->{page};
-    pass and return false unless grep /$page/, @pages;
-    template $page;
 };
 
 get '/donate/thanks' => sub {
     template 'thanks';
 };
-
 
 true;
