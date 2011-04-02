0536aa091ed7f4c3ef549a83198094bb49e74371
David Precious <davidp@preshweb.co.uk>
2011-04-02 21:26:43 +0100

    Automatically determine latest stable version.
    Find out what the latest stable version on CPAN is, and use the appropriate
    download URL and version in button text.

--- lib/PerlDancer.pm
+++ lib/PerlDancer.pm
@@ -2,15 +2,48 @@ package PerlDancer;
 
 use Dancer ':syntax';
 use Template;
+use LWP::Simple ();
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    template 'index', { latest => latest_version() };
 };
 
 get '/donate/thanks' => sub {
     template 'thanks';
 };
 
+
+# Find the latest stable version on Github.  Cache it for 10 minutes, to avoid
+# hitting it every single time
+{
+    my ($latest_version, $last_check);
+    sub latest_version {
+        if ($latest_version 
+            && time - $last_check < config->{cache_latest_version})
+        {
+            return $latest_version;
+        }
+
+        my $versions = from_json(LWP::Simple::get(
+            'http://search.cpan.org/api/dist/Dancer'
+        ));
+        my ($latest) = grep { $_->{latest} } @{ $versions->{releases} };
+        my $url = join '/', 'http://search.cpan.org/CPAN/authors/id',
+            substr($latest->{cpanid}, 0, 1),
+            substr($latest->{cpanid}, 0, 2),
+            $latest->{cpanid},
+            $latest->{archive};
+        my ($short_ver) = $latest->{version} =~ /^(\d+\.\d)/;
+        my $result =  {
+            version => $latest->{version},
+            short_version => $short_ver,
+            download_url => $url,
+        };
+        $latest_version = $result;
+        $last_check = time;
+        return $result;
+    }
+}
 true;
